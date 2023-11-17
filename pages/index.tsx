fdb81d7379c5ebb8c6fb06fc1c561adba5fe1b27
Jozsef Kiraly <jozsef.kiraly@byborgenterprises.com>
2023-11-17 13:20:05 +0100

    feat: Create initial folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from "react";
 import { Flex } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Home: FC = () => {
   return (
     <Flex w="100%" h="100%" flexDir="column">
-      Hello!
+      <Link href="/users">Open users page</Link>
     </Flex>
   );
 };
---
+++ pages/users.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import Main from "@/features/users/components/Main";
+
+const UsersPage: FC = () => {
+  return <Main />;
+};
+
+export default UsersPage;
