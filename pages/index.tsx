58865e943737252ce4ea5576b09de6ca20b90480
Andras Czipa <andras.czipa@byborgenterprises.com>
2023-11-20 10:50:44 +0100

    feat: fix package.json, remove unused import and props, remove box

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,23 +5,17 @@ import {
   Flex,
   VStack,
   Text,
-  Center,
   Button,
   Avatar,
-  Stack,
   StackDivider,
-  flexbox,
+  Wrap,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import UsersPage from "./users";
 
 const Home: FC = () => {
   return (
-    <VStack
-      divider={<StackDivider borderColor="gray.200" />}
-      spacing={4}
-      align="stretch"
-    >
+    <VStack spacing={4} align="stretch">
       <Container centerContent padding={10}>
         <Flex
           width="1140px"
@@ -31,23 +25,27 @@ const Home: FC = () => {
           padding={0}
           position="relative"
         >
-          <Flex display="flex" height="800px" bg="gray.300" width="200px">
-            <Box
-              justifyContent="center"
-              alignContent="center"
-              width="100%"
-              flexDirection="row"
-            >
+          <Flex height="800px" bg="gray.300" width="200px">
+            <Flex flexDirection="column">
               <Flex margin="12px" color="black" height="40px">
                 MentorApp
               </Flex>
-              <Flex justifyContent="center" alignContent="center" width="100%">
-                <Link href="/users">
-                  <Button colorScheme="blue" size="lg" width="150px">
-                    Users
-                  </Button>
-                </Link>
-              </Flex>
+              <Wrap spacing={4} justify={"center"}>
+                <Flex>
+                  <Link href="/users">
+                    <Button colorScheme="blue" size="lg" width="150px">
+                      Users
+                    </Button>
+                  </Link>
+                </Flex>
+                <Flex>
+                  <Link href="/users">
+                    <Button colorScheme="blue" size="lg" width="150px">
+                      teszt
+                    </Button>
+                  </Link>
+                </Flex>
+              </Wrap>
               <Flex
                 position="absolute"
                 bottom="15px"
@@ -62,13 +60,13 @@ const Home: FC = () => {
                     size="sm"
                     name="Dan Abrahmov"
                     src="https://bit.ly/dan-abramov"
-                  ></Avatar>
+                  />
                   <Text color="black" margin="auto">
                     Czipa Andras
                   </Text>
                 </Flex>
               </Flex>
-            </Box>
+            </Flex>
           </Flex>
           <Flex flex={1} width="auto" height="auto" bg="gray.700" />
         </Flex>
